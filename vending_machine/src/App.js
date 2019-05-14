c6047ee1da62752f93604c8e6f147e1be6927eb3
Subodh Sharma <subodhsharma@subodh.local>
2019-05-13 20:11:10 -0700

    progress 70%

--- vending_machine/src/App.js
+++ vending_machine/src/App.js
@@ -1,22 +1,28 @@
-import React, { Component } from 'react';
-import { Route } from "react-router-dom";
+import React, {Component} from "react";
+import { Route , Switch } from "react-router-dom";
 
 import VendingMachine from './VendingMachine';
 import Chips from './Chips';
 import Soda from './Soda';
 import Sardines from './Sardines';
+import Navbar from './Navbar';
+
 import './App.css';
 
 class App extends Component{
   render(){
     return (
+      
       <div className="App">
+      <Navbar />
+        <Switch>
         <Route exact path="/" render = {() => <VendingMachine/>} />
         <Route exact path="/Chips" render = {() => <Chips />} />
         <Route exact path="/Soda" render = {() => <Soda />} />
         <Route exact path="/Sardines" render = {() => <Sardines />} />
+        </Switch>
       </div>
-    )
+    );
   }
 }
 
--- vending_machine/src/Sardines.js
+++ vending_machine/src/Sardines.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
+import { Link } from "react-router-dom";
+import "./Sardines.css";
 
 class Sardines extends Component{
     render(){
         return (
-            <div className="Sardines">
-                This is also one of the nasty shit that comes inside the can.
+            <div 
+                className="Sardines"
+                style={{
+                    backgroundImage: "url(https://66.media.tumblr.com/d160dba2d7bdcaa6c972b477f6acd5d7/tumblr_njvnd9zupc1qm9k25o1_500.gif)"
+                }}>
+                <h1>you don't eat the sardines. the sardines, they eat you!</h1>
+                <Link to="/">Go Back</Link>
             </div>
-        )
+        );
     }
 }
 
