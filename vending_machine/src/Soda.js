b439132ce80ecdb073bb5e99629eb2446df3067a
Subodh Sharma <subodhsharma@subodh.local>
2019-05-13 19:43:04 -0700

    adding few Components

--- vending_machine/src/Soda.js
+++ vending_machine/src/Soda.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import sodaImg from "./Soda.png";
+import './Soda.css';
 
 class Soda extends Component{
     render(){
         return (
             <div className="Soda">
-                This is the worst shit, which is of course soda but we fk with it!!
+            <img src={sodaImg} alt='coca cola can' />
+            <div>
+                <h1>Soda is my favorite</h1>
+                <h2>But now I drink sparkling water instead.</h2>
+                <p> I still miss Soda.</p>
             </div>
-        )
+            <img src={sodaImg} alt='coca cola can' />
+                <Link to='/'>Go Back</Link>
+            </div>
+        );
     }
 }
 export default Soda;
--- vending_machine/src/VendingMachine.js
+++ vending_machine/src/VendingMachine.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './VendingMachine.css';
 
 class VendingMachine extends React.Component{
     render(){
         return (
-            <div className="VendingMachine">
+            <div 
+                className="VendingMachine">
+                
                 <h1>This is the Vending Machine Bitch!!!</h1>
+                <Link to="/soda">Soda</Link>
+                <Link to="/chips">Chips</Link>
+                <Link to="/sardines">Sardines</Link>
+
             </div>
         );
     }
